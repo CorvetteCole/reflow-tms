174acd728b8318d0497fa6d3286fe8debc74ef1c
Cole Gerdemann <cole@coleg.dev>
2023-10-31 14:55:12 -0400

    fix status json being too small and state being cut off

--- src/status.cpp
+++ src/status.cpp
@@ -1,13 +1,13 @@
 #include "status.h"
 
-StaticJsonDocument<96> Status::toJson() const {
-  StaticJsonDocument<96> statusJson;
+StaticJsonDocument<128> Status::toJson() const {
+  StaticJsonDocument<128> statusJson;
   statusJson[F("target")] = targetTemperature;
   statusJson[F("current")] = currentTemperature;
+  statusJson[F("state")] = stateStrings[state];
   statusJson[F("top")] = topHeatDutyCycle;
   statusJson[F("bottom")] = bottomHeatDutyCycle;
   statusJson[F("door_open")] = isDoorOpen;
   statusJson[F("error")] = error;
-  statusJson[F("state")] = stateStrings[state];
   return statusJson;
 }
--- src/status.h
+++ src/status.h
@@ -15,10 +15,10 @@ public:
   State state = State::IDLE;
   uint8_t error = 0;
 
-  StaticJsonDocument<96> toJson() const;
+  StaticJsonDocument<128> toJson() const;
 
 private:
-  const char *const stateStrings[4] = {"IDLE", "HEATING", "COOLING", "FAULT"};
+  const char *const stateStrings[4] = {"idle", "heating", "cooling", "fault"};
 };
 
 #endif // REFLOW_TMS_STATUS_H
