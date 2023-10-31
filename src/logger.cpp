dc30a5693d40f539ba0deb7715efd7616f647b9c
Cole Gerdemann <cole@coleg.dev>
2023-10-31 13:32:57 -0400

    remove String object usage

--- src/logger.cpp
+++ src/logger.cpp
@@ -1,27 +1,25 @@
 #include "logger.h"
 
-void Logger::debug(const String &message) const {
-  log(LogLevel::DEBUG, message);
-}
+void Logger::debug(const char *message) const { log(LogLevel::DEBUG, message); }
 void Logger::debug(const __FlashStringHelper *message) const {
   log(LogLevel::DEBUG, message);
 }
-void Logger::info(const String &message) const { log(LogLevel::INFO, message); }
+void Logger::info(const char *message) const { log(LogLevel::INFO, message); }
 void Logger::info(const __FlashStringHelper *message) const {
   log(LogLevel::INFO, message);
 }
-void Logger::warn(const String &message) const { log(LogLevel::WARN, message); }
+void Logger::warn(const char *message) const { log(LogLevel::WARN, message); }
 void Logger::warn(const __FlashStringHelper *message) const {
   log(LogLevel::WARN, message);
 }
-void Logger::error(const String &message) const {
+void Logger::error(const char *message) const {
   log(LogLevel::CRITICAL, message);
 }
 void Logger::error(const __FlashStringHelper *message) const {
   log(LogLevel::CRITICAL, message);
 }
 
-void Logger::log(LogLevel severity, const String &message) const {
+void Logger::log(LogLevel severity, const char *message) const {
   if (severity >= logLevel) {
     StaticJsonDocument<256> logJson;
     logJson["time"] = millis();
--- src/logger.h
+++ src/logger.h
@@ -4,30 +4,30 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 
-enum LogLevel { DEBUG = 0, INFO = 1, WARN = 2, CRITICAL = 3 };
+enum LogLevel{ DEBUG = 0, INFO = 1, WARN = 2, CRITICAL = 3 };
 
 class Logger {
 
 public:
   LogLevel logLevel = LogLevel::INFO;
 
-  void debug(const String &message) const;
+  void debug(const char *message) const;
 
   void debug(const __FlashStringHelper *message) const;
 
-  void info(const String &message) const;
+  void info(const char *message) const;
 
   void info(const __FlashStringHelper *message) const;
 
-  void warn(const String &message) const;
+  void warn(const char *message) const;
 
   void warn(const __FlashStringHelper *message) const;
 
-  void error(const String &message) const;
+  void error(const char *message) const;
 
   void error(const __FlashStringHelper *message) const;
 
-  void log(LogLevel severity, const String &message) const;
+  void log(LogLevel severity, const char *message) const;
 
   void log(LogLevel severity, const __FlashStringHelper *message) const;
 
