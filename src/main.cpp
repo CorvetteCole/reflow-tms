d3b1c63d2c65f41b20e3866cf08689f7774c5875
Cole Gerdemann <cole@coleg.dev>
2023-11-01 18:44:51 -0400

    initial step-response-generator

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <ArduinoJson.h>
 #include <QuickPID.h>
 #include <sTune.h>
+
 #define USE_TIMER_1 true
 #define USING_MICROS_RESOLUTION true
 
@@ -16,34 +17,46 @@
 // Don't change these numbers to make higher Timer freq. System can hang
 #define HW_TIMER_INTERVAL_FREQ 10000L
 
-// user settings
-uint32_t settleTimeSec = 10;
-uint32_t testTimeSec = 500; // runPid interval = testTimeSec / samples
-const uint16_t samples = 500;
-const float inputSpan = 350;
-const float outputSpan = 100;
-float outputStart = 0;
-float outputStep = 100;
-float tempLimit = 300;
-bool startup = true;
-
 AVR_Slow_PWM heatingElementPwm;
 
 Status status;
-
-float pidCurrentTemperature, pidTargetTemperature = 130, pidHeatDutyCycle = 0,
-                             Kp, Ki, Kd;
-
-sTune tuner = sTune(&pidCurrentTemperature, &pidHeatDutyCycle, sTune::Mixed_PID,
-                    sTune::directIP, sTune::printALL);
-QuickPID heatingElementPid(&pidCurrentTemperature, &pidHeatDutyCycle,
-                           &pidTargetTemperature);
 
 Adafruit_MAX31865 max31865 = Adafruit_MAX31865(CS_PIN, DI_PIN, DO_PIN, CLK_PIN);
 
 Logger logger = Logger(LogLevel::CRITICAL);
 
 void pwmTimerHandler() { heatingElementPwm.run(); }
+
+uint8_t lastHeatDutyCycle = 0;
+uint32_t lastSentPidTunerStatus = 0;
+
+// target temperatures used to create step response
+static const int targetTemperatureCurvePoints[] = {25,  100, 150, 183,
+                                                   235, 183, 150};
+
+static const int numTargetTemperatureCurvePoints =
+    sizeof(targetTemperatureCurvePoints) /
+    sizeof(targetTemperatureCurvePoints[0]);
+
+int targetTemperatureCurveIndex = 0;
+
+// max difference between last recorded points before the temperature is
+// considered stable
+static const float stabilizedThreshold = 0.5f;
+
+// number of points to consider when determining if the temperature is stable
+static const int stabilizationWindow = 4;
+
+// allowable difference between target and current temperature to be considered
+// "complete"
+static const float targetTemperatureAchievedThreshold = 2;
+
+// use PID_TUNER_SEND_INTERVAL as timing between points
+
+// float previousTemperaturePoints[stabilizationWindow] = {0};
+
+// circular buffer of previous temperature points
+float previousTemperaturePoints[stabilizationWindow] = {0};
 
 void immediateStop() {
   heatingElementPwm.modifyPWMChannel(0, TOP_HEATING_ELEMENT_PIN,
@@ -73,75 +86,6 @@ void enterErrorState(uint8_t error) {
     logger.error(ovenErrorToString(error));
     sendStatus(); // immediately update status
   }
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  logger.info(F("Starting thermal management system..."));
-  //  logger.info(BOARD_NAME);
-
-  logger.debug(F("Initializing ITimer1..."));
-
-  ITimer1.init();
-
-  logger.debug(F("Attaching ITimer1 interrupt..."));
-
-  if (ITimer1.attachInterrupt(HW_TIMER_INTERVAL_FREQ, pwmTimerHandler)) {
-    logger.debug(F("Starting ITimer1 OK"));
-  } else {
-    logger.error(
-        F("Can't set ITimer1 correctly. Select another freq. or timer"));
-  }
-
-  // initialize built-in LED pin as an output (will blink on heartbeat)
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(FAN_PIN, OUTPUT);
-  pinMode(TOP_HEATING_ELEMENT_PIN, OUTPUT);
-  pinMode(BOTTOM_HEATING_ELEMENT_PIN, OUTPUT);
-
-  // initialize door sensor pin as an input
-  pinMode(DOOR_PIN, INPUT_PULLUP);
-  pinMode(RESET_PIN, INPUT_PULLUP);
-
-  logger.debug(F("Attaching interrupts..."));
-
-  //  attachInterrupt(digitalPinToInterrupt(RESET_PIN), resetFunc, FALLING);
-
-  logger.debug(F("Initializing PWM..."));
-
-  // initialize both HEATING element PWM interfaces, set duty cycle to 0
-  heatingElementPwm.setPWM(TOP_HEATING_ELEMENT_PIN,
-                           HEATING_ELEMENT_PWM_FREQUENCY, 0);
-  heatingElementPwm.setPWM(BOTTOM_HEATING_ELEMENT_PIN,
-                           HEATING_ELEMENT_PWM_FREQUENCY, 0);
-  heatingElementPwm.setPWM(LED_BUILTIN, HEATING_ELEMENT_PWM_FREQUENCY, 0);
-
-  //  heatingElementPwm.disableAll(); // disable timers while we aren't using
-  //  them
-
-  logger.debug(F("Initializing PID..."));
-
-  //  heatingElementPid.SetOutputLimits(0, 100);
-  //  heatingElementPid.SetTunings(TOP_HEATING_ELEMENT_KP,
-  //  TOP_HEATING_ELEMENT_KI,
-  //                               TOP_HEATING_ELEMENT_KD);
-
-  logger.debug(F("Initializing MAX31865_3WIRE..."));
-
-  max31865.begin(MAX31865_3WIRE);
-
-  logger.info(F("Thermal management system started"));
-
-  heatingElementPid.SetOutputLimits(0, 100);
-
-  tuner.Configure(inputSpan, outputSpan, outputStart, outputStep, testTimeSec,
-                  settleTimeSec, samples);
-  tuner.SetEmergencyStop(tempLimit);
-
-  status.targetTemperature = pidTargetTemperature;
-
-  delay(3000);
 }
 
 /// Reads the temperature sensor, updating the status struct accordingly.
@@ -194,18 +138,88 @@ void readTemperature() {
   }
 }
 
-uint8_t lastHeatDutyCycle = 0;
-uint32_t lastSentStatus = 0;
+void setup() {
+  Serial.begin(115200);
 
-void loop() {
-  // send status
-  if (millis() - lastSentStatus > STATUS_SEND_INTERVAL) {
-//    readTemperature();
-    sendStatus();
-    lastSentStatus = millis();
+  // wait for serial port to connect
+  while (!Serial || !Serial.available()) {
+    delay(100);
   }
 
-//    return;
+  logger.info(F("Starting thermal management system..."));
+  //  logger.info(BOARD_NAME);
+
+  logger.debug(F("Initializing ITimer1..."));
+
+  ITimer1.init();
+
+  logger.debug(F("Attaching ITimer1 interrupt..."));
+
+  if (ITimer1.attachInterrupt(HW_TIMER_INTERVAL_FREQ, pwmTimerHandler)) {
+    logger.debug(F("Starting ITimer1 OK"));
+  } else {
+    logger.error(
+        F("Can't set ITimer1 correctly. Select another freq. or timer"));
+  }
+
+  // initialize built-in LED pin as an output (will blink on heartbeat)
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(FAN_PIN, OUTPUT);
+  pinMode(TOP_HEATING_ELEMENT_PIN, OUTPUT);
+  pinMode(BOTTOM_HEATING_ELEMENT_PIN, OUTPUT);
+
+  // initialize door sensor pin as an input
+  pinMode(DOOR_PIN, INPUT_PULLUP);
+  pinMode(RESET_PIN, INPUT_PULLUP);
+
+  logger.debug(F("Attaching interrupts..."));
+
+  //  attachInterrupt(digitalPinToInterrupt(RESET_PIN), resetFunc, FALLING);
+
+  logger.debug(F("Initializing PWM..."));
+
+  // initialize both HEATING element PWM interfaces, set duty cycle to 0
+  heatingElementPwm.setPWM(TOP_HEATING_ELEMENT_PIN,
+                           HEATING_ELEMENT_PWM_FREQUENCY, 0);
+  heatingElementPwm.setPWM(BOTTOM_HEATING_ELEMENT_PIN,
+                           HEATING_ELEMENT_PWM_FREQUENCY, 0);
+  heatingElementPwm.setPWM(LED_BUILTIN, HEATING_ELEMENT_PWM_FREQUENCY, 0);
+
+  logger.debug(F("Initializing MAX31865_3WIRE..."));
+
+  max31865.begin(MAX31865_3WIRE);
+
+  logger.info(F("Thermal management system started"));
+
+  logger.warn("starting in 3 seconds...");
+  readTemperature();
+  status.targetTemperature =
+      targetTemperatureCurvePoints[targetTemperatureCurveIndex];
+  delay(3000);
+}
+
+void loop() {
+
+  // send status
+  if (millis() - lastSentPidTunerStatus > PID_TUNER_SEND_INTERVAL) {
+    // record current temperature to buffer
+    for (int i = 0; i < stabilizationWindow - 1; i++) {
+      previousTemperaturePoints[i] = previousTemperaturePoints[i + 1];
+    }
+    previousTemperaturePoints[stabilizationWindow - 1] =
+        status.currentTemperature;
+
+    readTemperature();
+
+    // send time, current duty cycle, and current temperature
+    // time should be in seconds with decimal
+    Serial.print(millis() / 1000.0f);
+    Serial.print(",");
+    Serial.print(status.heatDutyCycle);
+    Serial.print(",");
+    Serial.println(status.currentTemperature);
+    lastSentPidTunerStatus = millis();
+  }
 
   if (status.state == State::FAULT) {
     // if we're in a fault state, don't do anything
@@ -213,51 +227,54 @@ void loop() {
   }
 
   status.isDoorOpen = !digitalRead(DOOR_PIN);
-//  if (status.state == State::HEATING && status.isDoorOpen) {
-//    enterErrorState(ERROR_DOOR_OPENED_DURING_HEATING);
-//  }
-
-  switch (tuner.Run()) {
-  case sTune::sample:
-    readTemperature();
-    pidCurrentTemperature = status.currentTemperature;
-    //    tuner.plotter(pidCurrentTemperature, pidHeatDutyCycle,
-    //    pidTargetTemperature,
-    //                  0.5f, 3);
-    break;
-  case sTune::tunings:
-    tuner.GetAutoTunings(&Kp, &Ki, &Kd); // sketch variables updated by sTune
-    //    heatingElementPid.SetOutputLimits(0, outputSpan * 0.1);
-    //    heatingElementPid.SetSampleTimeUs(100000);
-    pidHeatDutyCycle = outputStep;
-    heatingElementPid.SetMode(
-        QuickPID::Control::automatic); // the PID is turned on
-    heatingElementPid.SetProportionalMode(QuickPID::pMode::pOnErrorMeas);
-    heatingElementPid.SetAntiWindupMode(QuickPID::iAwMode::iAwCondition);
-    heatingElementPid.SetTunings(Kp, Ki, Kd); // update PID with the new tunings
-    break;
-
-  case sTune::runPid:
-    if (startup &&
-        pidCurrentTemperature > pidTargetTemperature - 10) { // reduce overshoot
-      startup = false;
-      pidHeatDutyCycle -= 9;
-      heatingElementPid.SetMode(QuickPID::Control::manual);
-      heatingElementPid.SetMode(QuickPID::Control::automatic);
-    }
-    readTemperature();
-    pidCurrentTemperature = status.currentTemperature;
-    heatingElementPid.Compute();
-    //    tuner.plotter(pidCurrentTemperature, pidHeatDutyCycle,
-    //    pidTargetTemperature,
-    //                  0.5f, 3);
-    break;
+  if (status.state == State::HEATING && status.isDoorOpen) {
+    enterErrorState(ERROR_DOOR_OPENED_DURING_HEATING);
   }
 
-  status.heatDutyCycle = static_cast<uint8_t>(pidHeatDutyCycle);
+  // calculate whether the temperature is stable
+  bool isTemperatureStable = true;
+  for (int i = 0; i < stabilizationWindow - 1; i++) {
+    if (abs(previousTemperaturePoints[i] - previousTemperaturePoints[i + 1]) >
+        stabilizedThreshold) {
+      isTemperatureStable = false;
+      break;
+    }
+  }
+
+  if (status.state == State::IDLE && isTemperatureStable &&
+      abs(status.currentTemperature - status.targetTemperature) <=
+          targetTemperatureAchievedThreshold) {
+    // we are waiting, the temperature is stable, and we have achieved the
+    // current target, so we can move onto the next point
+    targetTemperatureCurveIndex++;
+    if (targetTemperatureCurveIndex < numTargetTemperatureCurvePoints) {
+      status.targetTemperature =
+          targetTemperatureCurvePoints[targetTemperatureCurveIndex];
+
+      // determine whether we are heating or cooling
+      if (status.targetTemperature > status.currentTemperature) {
+        status.state = State::HEATING;
+      }
+
+    } else {
+      // we have reached the end of the curve, so we are done
+      status.state = State::IDLE;
+      status.heatDutyCycle = 0;
+      immediateStop();
+    }
+  }
+
+  if (status.state == State::HEATING) {
+    if (status.currentTemperature < status.targetTemperature) {
+      status.heatDutyCycle = 100;
+    } else {
+      status.state = State::IDLE;
+      status.heatDutyCycle = 0;
+    }
+  }
+
 
   if (status.heatDutyCycle != lastHeatDutyCycle) {
-    //    logger.debug(F("Updating PWM"));
     // set PWM
     heatingElementPwm.modifyPWMChannel(0, TOP_HEATING_ELEMENT_PIN,
                                        HEATING_ELEMENT_PWM_FREQUENCY,
